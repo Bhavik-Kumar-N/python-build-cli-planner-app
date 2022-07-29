eebb820a693ee68d94e726807b3c8be0333e6ded
Bhavik Kumar N <bhavik.n@stl.tech>
2022-07-30 02:57:46 +0530

    Build Your App (6)

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -32,7 +32,7 @@ class DateReminder(DeadlinedReminder):
         self.date=parse(date, dayfirst=True)
 
     def __iter__(self):
-        return iter([self.text. self.date.isoformat()])
+        return iter([self.text, self.date.isoformat()])
 
     def is_due(self):
         return self.date<=datetime.now()
